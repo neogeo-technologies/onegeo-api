06d5339fa4838443e01acdee10c629e369e9ec1a
chakibBH <c.benhabib@neogeo-technologies.fr>
2017-05-04 09:49:08 +0200

    dev

--- admin.py
+++ admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import Source, Resource, Context, Filter, Analyzer
+from .models import Source, Resource, Context, Filter, Analyzer, Tokenizer, SearchModel, Task
 
 admin.site.register(Source)
 admin.site.register(Resource)
 admin.site.register(Context)
 admin.site.register(Filter)
 admin.site.register(Analyzer)
+admin.site.register(Tokenizer)
+admin.site.register(SearchModel)
+admin.site.register(Task)
