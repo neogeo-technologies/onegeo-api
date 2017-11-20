efaebdcdd8779c6de9e5eb2a1e5c9620cf9248a2
chakibBH <cbenhabib@neogeo.fr>
2017-11-20 12:06:10 +0100

    dev

--- onegeo_api/admin.py
+++ onegeo_api/admin.py
@@ -1,18 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-from .models import Source, Resource, Context, Filter, Analyzer, Tokenizer, SearchModel, Task
+from .models import Analyzer
+from .models import Context
+from .models import Filter
+from .models import Resource
+from .models import SearchModel
+from .models import Source
+from .models import Tokenizer
+# from .models import Task
 
-admin.site.register(Source)
-admin.site.register(Resource)
-admin.site.register(Context)
-admin.site.register(Filter)
-admin.site.register(Analyzer)
-admin.site.register(Tokenizer)
+admin.site.unregister(Group)
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ['start_date', 'success']
-    ordering = ['start_date']
+
+@admin.register(Analyzer)
+class AnalyzerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+
+@admin.register(Context)
+class ContextAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+
+@admin.register(Filter)
+class FilterAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+
+@admin.register(Tokenizer)
+class TokenizerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+
+# @admin.register(Task)
+# class TaskAdmin(admin.ModelAdmin):
+#     list_display = ['start_date', 'success']
+#     ordering = ['start_date']
 
 
 @admin.register(SearchModel)
