f9c0f02437752dfe58221d3a436af70e4640c7a7
m431m <m431m@tutanota.com>
2018-08-07 16:25:43 +0200

    Added update_indexes cmd

---
+++ onegeo_api/management/commands/update_indexes.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2017-2018 Neogeo-Technologies.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from onegeo_api.celery_tasks import indexing
+from onegeo_api.models import IndexProfile
+from uuid import uuid4
+
+
+class Command(BaseCommand):
+
+    help = 'Update indexes'
+
+    def handle(self, *args, **kwargs):
+        for instance in IndexProfile.objects.all():
+            if self.is_index_to_update(instance):
+                self.update_index(instance)
+
+    def is_index_to_update(self, instance):
+        now = timezone.now()
+        return {
+            'never': False,
+            'daily': True,
+            'weekly': now.isoweekday() == 1,
+            'monthly': now.day == 1,
+            }.get(instance.reindex_frequency, False)
+
+    def update_index(self, instance):
+        task_id = uuid4()
+        indexing.apply_async(
+            kwargs={'alias': instance.alias.pk,
+                    'force_update': True,
+                    'index_profile': instance.pk,
+                    'resource_ns': 'index',
+                    'user': instance.user.pk},
+            task_id=str(task_id))
